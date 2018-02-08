646cd64af2c91748a735f4cf3118240c4570d922
kshitij <kshitij.kumar@lyxellabs.com>
2018-02-08 22:05:01 +0530

    frontend tweaks

--- frontend/src/components/formSegment.vue
+++ frontend/src/components/formSegment.vue
@@ -3,28 +3,51 @@
   <fieldset>
     <legend>Form Builder</legend>
     <div class="row">
-      <div class="column-sm-4">
+      <div class="col-sm-6">
         <div class="input-group fluid">
           <label for="username">Field</label>
           <input type="text" placeholder="Type Field Name" v-model="rs.title"/>
         </div>
       </div>
-      <div class="column-sm-10">
+      <div class="col-sm-6">
         <div class="input-group fluid">
           <label for="pwd">Type</label>     
-            <select>
-              <option>{{rs.type}}</option>
-              <option>Checkbox</option>
-              <option>TextBox</option>
+            <select v-model="rs.type">
+              <option>NUMBER</option>
+              <option>TEXT</option>
+              <option>DATE</option>
             </select>
         </div>
       </div>
     </div>
-  </fieldset>
-  
 
+    <div class="row" v-if="rs.type=='TEXT'">
+      <div class="col-sm-6">
+        <div class="input-group fluid">
+          <label for="input">Input</label>
+          <input type="text"></input>
+        </div>
+      </div>
+    </div>
+
+    <div class="row" v-if="rs.type=='DATE'">
+      <div class="col-sm-6">
+        <div class="input-group fluid">
+          <label for="input">Input</label>
+          <input type="Date"></input>
+        </div>
+      </div>
+    </div>
+    <div class="row" v-if="rs.type=='NUMBER'">
+      <div class="col-sm-6">
+        <div class="input-group fluid">
+          <label for="input">Input</label>
+          <input type="number"></input>
+        </div>
+      </div>
+    </div>
+  </fieldset>
 </form>
-</div>
 </template>
 
 <script lang="ts">
@@ -36,6 +59,7 @@ export default class formSegment extends Vue {
   
   @Prop({default: {}}) 
   private rs: any;
+  private widgetTypeSeleted:string='';
   // @Prop() public ds: { label: string, value: string }[] = [];
   // public lds: { label: string, value: string }[] = [];
   // public asyncOK(): void {
